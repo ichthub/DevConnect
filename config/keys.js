3e8c2f20e85bf5d5cc543e9bdd57e512511f55ba
Hassan <hassanhoumaid92@gmail.com>
2018-05-31 00:38:33 +0000

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-// module.exports behaves like a container of exported values.
-module.exports = {
-  mongoURI: 'mongodb://ichthub:password@ds139890.mlab.com:39890/devconnecter',
-  SecretKey: 'fdfds45SDF3456GHDHJddfgdh'
-};
