4d9cab25a408f731dd104ce19eba9b11a5ce7585
Hassan <hassanhoumaid92@gmail.com>
2018-07-27 03:24:40 +0000

    test

--- client/src/App.js
+++ client/src/App.js
@@ -65,39 +65,27 @@ class App extends Component {
                 <Route exact path="/profile/:handle" component={Profile} />
                 <Switch>
                   <PrivateRoute exact path="/dashboard" component={Dashboard} />
-                </Switch>
-                <Switch>
                   <PrivateRoute
                     exact
                     path="/create-profile"
                     component={CreateProfile}
                   />
-                </Switch>
-                <Switch>
                   <PrivateRoute
                     exact
                     path="/edit-profile"
                     component={EditProfile}
                   />
-                </Switch>
-                <Switch>
                   <PrivateRoute
                     exact
                     path="/add-experience"
                     component={AddExperience}
                   />
-                </Switch>
-                <Switch>
                   <PrivateRoute
                     exact
                     path="/add-education"
                     component={AddEducation}
                   />
-                </Switch>
-                <Switch>
                   <PrivateRoute exact path="/feed" component={Posts} />
-                </Switch>
-                <Switch>
                   <PrivateRoute exact path="/post/:id" component={Post} />
                 </Switch>
                 <Route path="/not-found" component={NotFound} />
